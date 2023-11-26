adb3ddffc5df29d8f868c40360a893746e601e1e
Jiji James <jijijames@crsoftware.com>
2023-11-26 17:49:40 -0500

    lastest stow fixes

---
+++ 00_dot_files_setup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+files=(
+    ".gitconfig"
+    ".bash_profile"
+    ".bashrc"
+    ".ideavimrc"
+    ".vimrc"
+    ".gitconfig"
+    ".gitattributes"
+    ".gitignore"
+)
+
+folders=(
+
+)
+
+echo "Removing existing config files"
+for f in "${files[@]}"; do
+  rm -f "$HOME/$f" || true
+done
+
+# Create the folders to avoid symlinking folders
+for d in "${folders[@]}"; do
+  rm -rf "${HOME:?}/$d" || true
+  mkdir -p "$HOME/$d"
+done
+
+to_stow="$(find stow -maxdepth 1 -type d -mindepth 1 | awk -F "/" '{print $NF}' ORS=' ')"
+IFS=' ' read -ra stow_array <<< "$to_stow"
+
+for s in "${stow_array[@]}"; do
+  echo "Stowing: $s"
+  stow -d stow --verbose 1 --target "$HOME" "$s"
+done
