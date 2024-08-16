d1cd39afcd841d6fa9e8d5d52655d52c34befc4d
Jiji James <jijijames@crsoftware.com>
2024-08-16 15:06:59 -0400

    Update intellij install

--- ubuntu/intellij.sh
+++ ubuntu/intellij.sh
@@ -3,13 +3,15 @@
 version='2024.2.0.1'
 
 mkdir ~/.jetbrains
+rm -f ~/.jetbrains/idea
+
+sudo apt install -y gnome-software
 
 # Find download link from https://www.jetbrains.com/idea/download/other.html
-wget https://download.jetbrains.com/idea/ideaIU-$version.tar.gz
-mv ~/ideaIU-$version.tar.gz ~/.jetbrains
+wget https://download.jetbrains.com/idea/ideaIU-$version.tar.gz -P ~/.jetbrains
 
 tar -zxvf ~/.jetbrains/ideaIU-$version.tar.gz -C ~/.jetbrains
 rm ~/.jetbrains/ideaIU-$version.tar.gz 
 
 ln -s ~/.jetbrains/idea-IU* ~/.jetbrains/idea
-
+ls -l ~/.jetbrains
