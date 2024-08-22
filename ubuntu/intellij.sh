d98942adeb972a1edbbcb2b7db9445d7e64c88b6
Jiji James <jijijames@crsoftware.com>
2024-08-22 15:31:45 -0400

    add toolbox

--- ubuntu/intellij.sh
+++ ubuntu/intellij.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version='2024.2.0.1'
+idea_version='2024.2.0.1'
 
 mkdir ~/.jetbrains
 rm -f ~/.jetbrains/idea
@@ -8,10 +8,16 @@ rm -f ~/.jetbrains/idea
 sudo apt install -y gnome-software
 
 # Find download link from https://www.jetbrains.com/idea/download/other.html
-wget https://download.jetbrains.com/idea/ideaIU-$version.tar.gz -P ~/.jetbrains
+wget https://download.jetbrains.com/idea/ideaIU-$idea_version.tar.gz -P ~/.jetbrains
 
-tar -zxvf ~/.jetbrains/ideaIU-$version.tar.gz -C ~/.jetbrains
-rm ~/.jetbrains/ideaIU-$version.tar.gz 
+tar -zxvf ~/.jetbrains/ideaIU-$idea_version.tar.gz -C ~/.jetbrains
+rm -f ~/.jetbrains/ideaIU-$idea_version.tar.gz 
 
 ln -s ~/.jetbrains/idea-IU* ~/.jetbrains/idea
 ls -l ~/.jetbrains
+
+# https://www.jetbrains.com/toolbox-app/download/other.html
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.4.2.32922.tar.gz -P ~/.jetbrains
+tar -zxvf ~/.jetbrains/jetbrains-toolbox-2.4.2.32922.tar.gz -C ~/.jetbrains
+rm -f ~/.jetbrains/jetbrains-toolbox-2.4.2.32922.tar.gz
+ln -s ~/.jetbrains/jetbrains-toolbox-2.4.2.32922 ~/.jetbrains/toolbox
