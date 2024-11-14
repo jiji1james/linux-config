189b9d61e9669b75e54fd3836342e2107e09ee45
Jiji James <jijijames@crsoftware.com>
2024-11-14 10:06:46 -0500

    minor updates

--- ubuntu/intellij.sh
+++ ubuntu/intellij.sh
@@ -1,94 +1,105 @@
 #!/usr/bin/env bash
 
-idea_version='2024.2.1'
+idea_version='2024.3'
 use_ultimate='false'
 use_community='false'
 remove_existing='false'
 install_idea='false'
 
 help_message() {
-  echo
-  echo "Usage: $(basename $0) [-v 2024.2.0.2] [-u] [-c] [-r] [-h]"
-  echo "options:"
-  echo "h     Print the Help Message."
-  echo "v     Intellij Version."
-  echo "u     Use Ultimate Version."
-  echo "c     Use Community Version."
-  echo "r     Remove exising install."
-  echo
+	echo
+	echo "Usage: $(basename $0) [-v 2024.2.0.2] [-u] [-c] [-r] [-h]"
+	echo "options:"
+	echo "h     Print the Help Message."
+	echo "v     Intellij Version."
+	echo "u     Use Ultimate Version."
+	echo "c     Use Community Version."
+	echo "r     Remove exising install."
+	echo
 }
 
+# Add this check before the while loop
+if [ $# -eq 0 ]; then
+    help_message
+    exit 1
+fi
+
 while getopts ':v:curh' opt; do
-  case "$opt" in
-    v)
-      build_version="$OPTARG"
-      echo "Using Build Number = ${OPTARG}"
-      ;;
+	case "$opt" in
+		v)
+			build_version="$OPTARG"
+			echo "Using Build Number = ${OPTARG}"
+			;;
 
-    u)
-      use_ultimate='true'
-      echo "Using Intellij Ultimate Version"
-      ;;
+		u)
+			use_ultimate='true'
+			echo "Using Intellij Ultimate Version"
+			;;
 
-    c)
-      use_community='true'
-      echo "Using Intellij Community Version"
-      ;;
+		c)
+			use_community='true'
+			echo "Using Intellij Community Version"
+			;;
 
-    r)
-      remove_existing='true'
-      echo "Removing existing Intellij from $HOME/.jetbrains"
-      ;;
+		r)
+			remove_existing='true'
+			echo "Removing existing Intellij from $HOME/.jetbrains"
+			;;
 
-    h)
-      help_message
-      exit 1
-      ;;
+		h)
+			help_message
+			exit 1
+			;;
 
-    :)
-      echo -e "Option requires an argument.\nUsage: $(basename $0) [-v 2024.2.0.2] [-u] [-h]"
-      exit 1
-      ;;
+		:)
+			echo -e "Option requires an argument.\nUsage: $(basename $0) [-v 2024.2.0.2] [-u] [-h]"
+			exit 1
+			;;
 
-    ?)
-      echo -e "Invalid command option.\nUsage: $(basename $0) [-v 2024.2.0.2] [-u] [-h]"
-      exit 1
-      ;;
-  esac
+		?)
+			echo -e "Invalid command option.\nUsage: $(basename $0) [-v 2024.2.0.2] [-u] [-h]"
+			exit 1
+			;;
+	esac
 done
 shift "$(($OPTIND -1))"
 
 if [[ "$remove_existing" == 'true' ]]; then
-    # Delete previous installs
-    rm -rf ~/.jetbrains
-elif [[ "$use_ultimate" == 'true' || "$use_community" == 'true' ]]; then
-    mkdir -p ~/.jetbrains/software
+	# Delete previous installs
+	rm -rf ~/.jetbrains/software
 
-    sysctl_file='/etc/sysctl.conf'
-    file_watches_string='fs.inotify.max_user_watches=1048576'
+	# Exit after removing
+	echo "Existing installs removed."
+	exit 0
+fi
 
-    if ! grep -q -F "$file_watches_string" "$sysctl_file"; then
-        echo "$file_watches_string" | sudo tee -a $sysctl_file
-        sudo sysctl -p # reload the config
-    fi
+if [[ "$use_ultimate" == 'true' || "$use_community" == 'true' ]]; then
+	mkdir -p ~/.jetbrains/software
 
-    # Install required software
-    sudo apt install -y gnome-software
+	sysctl_file='/etc/sysctl.conf'
+	file_watches_string='fs.inotify.max_user_watches=1048576'
+
+	if ! grep -q -F "$file_watches_string" "$sysctl_file"; then
+		echo "$file_watches_string" | sudo tee -a $sysctl_file
+		sudo sysctl -p # reload the config
+	fi
+
+	# Install required software
+	sudo apt install -y gnome-software
 fi
 
 # Find download link from https://www.jetbrains.com/idea/download/other.html
-
+echo "Installing version: $idea_version"
 if [[ "$use_ultimate" == 'true' ]]; then
-    wget https://download.jetbrains.com/idea/ideaIU-$idea_version.tar.gz -P ~/.jetbrains
-    tar -zxvf ~/.jetbrains/ideaIU-$idea_version.tar.gz -C ~/.jetbrains/software
-    rm -f ~/.jetbrains/ideaIU-$idea_version.tar.gz 
-    ln -s ~/.jetbrains/software/idea-IU* ~/.jetbrains/ideau
+	wget https://download.jetbrains.com/idea/ideaIU-$idea_version.tar.gz -P ~/.jetbrains
+	tar -zxvf ~/.jetbrains/ideaIU-$idea_version.tar.gz -C ~/.jetbrains/software
+	rm -f ~/.jetbrains/ideaIU-$idea_version.tar.gz 
+	ln -s ~/.jetbrains/software/idea-IU* ~/.jetbrains/ideau
 fi
 
 if [[ "$use_community" == 'true' ]]; then
-    wget https://download.jetbrains.com/idea/ideaIC-$idea_version.tar.gz -P ~/.jetbrains
-    tar -zxvf ~/.jetbrains/ideaIC-$idea_version.tar.gz -C ~/.jetbrains/software
-    rm -f ~/.jetbrains/ideaIC-$idea_version.tar.gz 
-    ln -s ~/.jetbrains/software/idea-IC* ~/.jetbrains/ideac
+	wget https://download.jetbrains.com/idea/ideaIC-$idea_version.tar.gz -P ~/.jetbrains
+	tar -zxvf ~/.jetbrains/ideaIC-$idea_version.tar.gz -C ~/.jetbrains/software
+	rm -f ~/.jetbrains/ideaIC-$idea_version.tar.gz 
+	ln -s ~/.jetbrains/software/idea-IC* ~/.jetbrains/ideac
 fi
-
--- ubuntu/toolbox.sh
+++ ubuntu/toolbox.sh
@@ -9,7 +9,7 @@ sysctl_file='/etc/sysctl.conf'
 file_watches_string='fs.inotify.max_user_watches=1048576'
 
 if ! grep -q -F "$file_watches_string" "$sysctl_file"; then
-	echo "$file_watches_string" | sudo tee -a $sysctl_file
+        echo "$file_watches_string" | sudo tee -a $sysctl_file
         sudo sysctl -p # reload the config
 fi
 
@@ -26,5 +26,4 @@ wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.4.2.32922.tar.gz
 tar -xzvf jetbrains-toolbox-2.4.2.32922.tar.gz -C .
 ln -sf ~/.jetbrains/software/toolbox/jetbrains-toolbox-2.4.2.32922 ~/.jetbrains/jetbrains-toolbox
 rm -f ~/.jetbrains/software/toolbox/jetbrains-toolbox-2.4.2.32922.tar.gz
-
 
