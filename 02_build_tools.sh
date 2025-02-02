69494fc10d4b4315e1ea3c30bcf47bc220cdf719
Jiji James <jijijames@crsoftware.com>
2025-02-02 16:22:14 -0500

    Updated linux versions

--- 02_build_tools.sh
+++ 02_build_tools.sh
@@ -2,22 +2,15 @@
 cwd=$(pwd)
 
 # Install AWS CLI
-echo ""
-echo ">>>> Installing AWS CLI Version 2"
-cd $HOME
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm -rf $HOME/aws
-rm -f awscliv2.zip
+brew install awscli
 
 # Install SDKMAN
 curl -s "https://get.sdkman.io" | bash
 source $HOME/.sdkman/bin/sdkman-init.sh
 
-sdk install java 8.0.422-amzn
-sdk install java 17.0.11-amzn
-sdk install java 21.0.4-amzn
+sdk install java 8.0.442-amzn
+sdk install java 17.0.14-amzn
+sdk install java 21.0.6-amzn
 
 sdk install ant 1.9.15
 sdk install maven 3.9.6
